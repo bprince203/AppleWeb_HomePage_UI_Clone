6942bf85e7c6c802b297b87f9232a6ed2965066b
Himanshu Bhardwaj <hiprince203@gmail.com>
2024-10-26 17:02:39 +0530

    Creating the bottom bar

---
+++ script.js
@@ -0,0 +1,16 @@
+// {/* <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+//     <script>
+//       var swiper = new Swiper('.scroll', {
+//         slidesPerView: 1,
+//         spaceBetween: 30,
+//         loop: true,
+//         pagination: {
+//           el: '.swiper-pagination',
+//           clickable: true,
+//         },
+//         navigation: {
+//           nextEl: '.swiper-button-next',
+//           prevEl: '.swiper-button-prev',
+//         },
+//       });
+//     </script> */}
